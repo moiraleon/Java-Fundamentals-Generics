bdcdd660f8d043ebf3a9fba2ef53c7e792fe1711
Moira Leon <moira.leon2@gmail.com>
2021-08-26 15:28:53 -0600

    Lambda Expressions

--- src/com/devmountain/java/fundamentals/LambdaExamples.java
+++ src/com/devmountain/java/fundamentals/LambdaExamples.java
@@ -10,9 +10,23 @@ public class LambdaExamples {
         final Person andAshley = new Person("and Ashley", 35);
         final Person demiLovato = new Person("Demi Lovato", 29);
 
-        Predicate<Person> isOld = person -> person.getAge(>80);
+//        Predicate<Person> isOld = person -> person.getAge(>80);
+//
+//        System.out.println(isOld.test(maryKate));
+//        System.out.println(isOld.test(mandAshley));
 
-        System.out.println(isOld.test(maryKate));
-        System.out.println(isOld.test(mandAshley));
+        //or using Java 8 Stream System
+
+//        Map<Boolean>, List<Person>> oldAndYoungPeople = for rewturning people old and young
+        Map<Boolean>, Long> oldAndYoungPeople = //counts individuals in each group
+            people.stream()
+                    .collect(partitioningBy(person-> person.getAge()>80,
+                            .counting()));
+
+        System.out.println(oldAndYoungPeople);
+//when havign errors with lambda expression check first to see that the target type is correct Map<Boolean>, Long>
+        //then revise type usage inside lambda expression person-> person.getAge()>80,
+
+
     }
 }
