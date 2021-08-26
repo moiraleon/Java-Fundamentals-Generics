12faef2ab8b1e60d21bab76fc509ffb630d4a203
Moira Leon <moira.leon2@gmail.com>
2021-08-26 16:02:26 -0600

    VarArgs

---
+++ src/com/devmountain/java/fundamentals/VarArgs.java
@@ -0,0 +1,8 @@
+package com.devmountain.java.fundamentals;
+
+public class VarArgs {
+}
+//arrayOf can be un safe at runtime and throw compile errors
+
+//documentaTION EXCEPTION works when code is safe and there is no heap pollution
+@SafeVarArgs
